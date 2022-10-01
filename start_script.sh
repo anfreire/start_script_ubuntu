cfdc72c4dac185d9ec624a03dcd506ddfbedd1f5
André Freire Ferreira <100360644+anfreire@users.noreply.github.com>
2022-10-01 19:22:05 +0100

    Add files via upload

---
+++ start_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sudo apt-get --with-new-pkgs upgrade
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt autoremove -y
+sudo snap refresh
+sudo flatpak update -y
+sudo apt full-upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
+printf "%s\n" "███████  ██████ ██████  ██ ██████  ████████     ██████  ██    ██    "
+printf "%s\n" "██      ██      ██   ██ ██ ██   ██    ██        ██   ██  ██  ██  ██ "
+printf "%s\n" "███████ ██      ██████  ██ ██████     ██        ██████    ████      "
+printf "%s\n" "     ██ ██      ██   ██ ██ ██         ██        ██   ██    ██    ██ "
+printf "%s\n" "███████  ██████ ██   ██ ██ ██         ██        ██████     ██       "
+printf "%s\n" "                                                                    "
+printf "%s\n" "                                                                    "
+printf "%s\n" " █████  ███    ██ ███████ ██████  ███████ ██ ██████  ███████        "
+printf "%s\n" "██   ██ ████   ██ ██      ██   ██ ██      ██ ██   ██ ██             "
+printf "%s\n" "███████ ██ ██  ██ █████   ██████  █████   ██ ██████  █████          "
+printf "%s\n" "██   ██ ██  ██ ██ ██      ██   ██ ██      ██ ██   ██ ██             "
+printf "%s\n" "██   ██ ██   ████ ██      ██   ██ ███████ ██ ██   ██ ███████       "
